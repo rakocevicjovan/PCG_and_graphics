3b743105c18c902b759b10d01662f2ad5a0c513e
Jovan <metalheadjohny@gmail.com>
2018-12-10 16:26:04 +0000

    Sorted out for submission

--- Engine/Renderer.cpp
+++ Engine/Renderer.cpp
@@ -31,95 +31,95 @@ bool Renderer::Initialize(int windowWidth, int windowHeight, HWND hwnd, InputMan
 	_deviceContext = _D3D->GetDeviceContext();
 
 
-	//this should not be here... but time waits for no man...
+	//model and shader loading should not be here... but time waits for no man...
 	///SHADER LOADING 
 	std::vector<std::wstring> names;
-	names.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/lightvs.hlsl");
-	names.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/lightps.hlsl");
+	names.push_back(L"lightvs.hlsl");
+	names.push_back(L"lightps.hlsl");
 	shaderLight.Initialize(_device, hwnd, names);
 	_shaders.push_back(shaderLight);
 
 	std::vector<std::wstring> wfsNames;
-	wfsNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/wireframe.vs");
-	wfsNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/wireframe.gs");
-	wfsNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/wireframe.ps");
+	wfsNames.push_back(L"wireframe.vs");
+	wfsNames.push_back(L"wireframe.gs");
+	wfsNames.push_back(L"wireframe.ps");
 	shaderWireframe.Initialize(_device, hwnd, wfsNames);
 
 	std::vector<std::wstring> hudNames;
-	hudNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/rekt.vs");
-	hudNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/rekt.ps");
+	hudNames.push_back(L"rekt.vs");
+	hudNames.push_back(L"rekt.ps");
 	shaderHUD.Initialize(_device, hwnd, hudNames);
 
 	std::vector<std::wstring> depthNames;
-	depthNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/depth.vs");
-	depthNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/depth.ps");
+	depthNames.push_back(L"depth.vs");
+	depthNames.push_back(L"depth.ps");
 	shaderDepth.Initialize(_device, hwnd, depthNames);
 
 	std::vector<std::wstring> projTexNames;
-	projTexNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/projectTex.vs");
-	projTexNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/projectTex.ps");
+	projTexNames.push_back(L"projectTex.vs");
+	projTexNames.push_back(L"projectTex.ps");
 	shaderPT.Initialize(_device, hwnd, projTexNames);
 
 	std::vector<std::wstring> shadowNames;
-	shadowNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/shadowvs.hlsl");
-	shadowNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/shadowps.hlsl");
+	shadowNames.push_back(L"shadowvs.hlsl");
+	shadowNames.push_back(L"shadowps.hlsl");
 	shaderShadow.Initialize(_device, hwnd, shadowNames);
 
 	std::vector<std::wstring> cubeMapNames;
-	cubeMapNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/cubemap.vs");
-	cubeMapNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/cubemap.ps");
+	cubeMapNames.push_back(L"cubemap.vs");
+	cubeMapNames.push_back(L"cubemap.ps");
 	shaderCM.Initialize(_device, hwnd, cubeMapNames);
 
 	std::vector<std::wstring> skyboxNames;
-	skyboxNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/skyboxvs.hlsl");
-	skyboxNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/skyboxps.hlsl");
+	skyboxNames.push_back(L"skyboxvs.hlsl");
+	skyboxNames.push_back(L"skyboxps.hlsl");
 	shaderSkybox.Initialize(_device, hwnd, skyboxNames);
 
 	std::vector<std::wstring> strifeNames;
-	strifeNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/strifevs.hlsl");
-	strifeNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/strifeps.hlsl");
+	strifeNames.push_back(L"strifevs.hlsl");
+	strifeNames.push_back(L"strifeps.hlsl");
 	shaderStrife.Initialize(_device, hwnd, strifeNames);
 
 	std::vector<std::wstring> waterNames;
-	waterNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/Watervs.hlsl");
-	waterNames.push_back(L"C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Engine/Waterps.hlsl");
+	waterNames.push_back(L"Watervs.hlsl");
+	waterNames.push_back(L"Waterps.hlsl");
 	shaderWater.Initialize(_device, hwnd, waterNames);
 	///SHADER LOADING DONE
 
 
 
 	///MODEL LOADING
-	modTerrain.LoadModel(_device, "C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Models/Terrain/NewTerTex.fbx", 50, 50);
+	modTerrain.LoadModel(_device, "../Models/Terrain/NewTerTex.fbx", 50, 50);
 	Math::Scale(modTerrain.transform, SVec3(2.f));
 	_terrainModels.push_back(&modTerrain);
 
-	modTreehouse.LoadModel(_device, "C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Models/Terrain/Treehouse/thouse(formats).fbx");
+	modTreehouse.LoadModel(_device, "../Models/Terrain/Treehouse/thouse(formats).fbx");
 	Math::SetTranslation(modTreehouse.transform, SVec3(0.0f, -50.f, -100.f));
 	SMatrix treehouseRotation = SMatrix::CreateFromAxisAngle(SVec3::Up, 30.f);
 	Math::SetRotation(modTreehouse.transform, treehouseRotation);
 	_terrainModels.push_back(&modTreehouse);
 
-	modBallStand.LoadModel(_device, "C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Models/ballstand.fbx");
+	modBallStand.LoadModel(_device, "../Models/ballstand.fbx");
 	SMatrix modBallStandRotation = SMatrix::CreateFromAxisAngle(SVec3::Right, PI * 0.5f);
 	Math::SetRotation(modBallStand.transform, modBallStandRotation);
 	Math::Scale(modBallStand.transform, SVec3(10.f));
 	Math::Translate(modBallStand.transform, SVec3(300.0f, -35.0f, -295.0f));
 
-	modBall.LoadModel(_device, "C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Models/ball.fbx");
+	modBall.LoadModel(_device, "../Models/ball.fbx");
 	Math::Scale(modBall.transform, SVec3(36.f));
 	Math::Translate(modBall.transform, modBallStand.transform.Translation() + SVec3(0.0f, 42.0f, 0.0f));
 
-	modStrife.LoadModel(_device, "C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Models/oohlalabox.fbx");
-	//Math::SetRotation(modStrife.transform, SMatrix::CreateFromAxisAngle(SVec3::Right, PI));
-	Math::Scale(modStrife.transform, SVec3(150.0f));	//50.f, 5.0f,
-	Math::Translate(modStrife.transform, SVec3(0.0f, 500.0f, 500.0f));
+	modStrife.LoadModel(_device, "../Models/oohlala.fbx");
+	Math::Scale(modStrife.transform, SVec3(150.0f));
+	Math::Translate(modStrife.transform, SVec3(-700.f, 200.0f, -700.0f));
 
-	modDepths.LoadModel(_device, "C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Models/WaterQuad.fbx");
+	modDepths.LoadModel(_device, "../Models/WaterQuad.fbx");
 	Math::Scale(modDepths.transform, SVec3(120.0f));
 	Math::Translate(modDepths.transform, SVec3(0.0f, -50.0f, 0.0f));
 
-	modSkybox.LoadModel(_device, "C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Models/Skysphere.fbx");
-	modWaterQuad.LoadModel(_device, "C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Models/WaterQuad.fbx");
+	modSkybox.LoadModel(_device, "../Models/Skysphere.fbx");
+	Math::Scale(modSkybox.transform, SVec3(10.0f));
+	modWaterQuad.LoadModel(_device, "../Models/WaterQuad.fbx");
 	///MODEL LOADING DONE
 
 
@@ -173,17 +173,17 @@ bool Renderer::Initialize(int windowWidth, int windowHeight, HWND hwnd, InputMan
 	///CUBE MAPS SETUP
 	cubeMapper.Init(_device);
 	shadowCubeMapper.Init(_device);
-	skyboxCubeMapper.LoadFromFiles(_device, "C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Textures/night.dds");
+	skyboxCubeMapper.LoadFromFiles(_device, "../Textures/night.dds");
 	///CUBE MAPS SETUP DONE
 
 
 
 	///NOISES
-	white.LoadFromFile("C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Textures/noiz.png");
+	white.LoadFromFile("../Textures/noiz.png");
 	white.Setup(_device);
-	perlin.LoadFromFile("C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Textures/strife.png");
+	perlin.LoadFromFile("../Textures/strife.png");
 	perlin.Setup(_device);
-	worley.LoadFromFile("C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Textures/worley.png");
+	worley.LoadFromFile("../Textures/worley.png");
 	worley.Setup(_device);
 	///NOISES DONE
 
@@ -192,7 +192,7 @@ bool Renderer::Initialize(int windowWidth, int windowHeight, HWND hwnd, InputMan
 	///TERRAIN GENERATION
 	/*
 	Texture t;
-	t.fileName = "C:/Users/Senpai/Documents/Visual Studio 2015/Projects/Lab 5 lighting/Engine/Textures/volcano.png";
+	t.fileName = "../Textures/volcano.png";
 	BitMapper bitMapper(t);
 	bitMapper.init(1, 2, 2, 0.3);	//g, w, l, h
 	if (bitMapper.createTerrain())
@@ -211,8 +211,6 @@ bool Renderer::Frame(float dTime){
 	
 	for (Camera& c : _cameras)
 		c.update(dTime);
-
-	Math::SetTranslation(modSkybox.transform, _cameras[0].GetCameraMatrix().Translation());
 
 	//OutputFPS(dTime);
 
@@ -246,6 +244,8 @@ bool Renderer::RenderFrame(float dTime){
 	_deviceContext->RSSetViewports(1, &(cubeMapper.cm_viewport));
 	cubeMapper.UpdateCams(modBall.transform.Translation());
 
+	SMatrix wot = DirectX::XMMatrixInverse(nullptr, cubeMapper.cameras[0]);
+	Math::SetTranslation(modSkybox.transform, wot.Translation());
 	for (int i = 0; i < 6; i++) {
 
 		_deviceContext->ClearRenderTargetView(cubeMapper.cm_rtv[i], cubeMapper.clearCol);
@@ -256,13 +256,25 @@ bool Renderer::RenderFrame(float dTime){
 		modTerrain.Draw(_deviceContext, shaderLight);
 		shaderLight.ReleaseShaderParameters(_deviceContext);
 
-
 		shaderLight.SetShaderParameters(_deviceContext, modTreehouse, cubeMapper.cameras[i], cubeMapper.lens, pointLight, _cameras[0].GetCameraMatrix().Translation(), dTime);
 		modTreehouse.Draw(_deviceContext, shaderLight);
 		shaderLight.ReleaseShaderParameters(_deviceContext);
-	}
-	///REFLECTION CUBE MAP DONE
 
+		_D3D->TurnOffCulling();
+		_D3D->SwitchDepthToLessEquals();
+		
+		
+		shaderSkybox.SetShaderParameters(_deviceContext, modSkybox, cubeMapper.cameras[i], cubeMapper.lens,
+			_cameras[0].GetCameraMatrix().Translation(), dTime, skyboxCubeMapper.cm_srv);
+		modSkybox.Draw(_deviceContext, shaderSkybox);
+		shaderSkybox.ReleaseShaderParameters(_deviceContext);
+		
+		_D3D->SwitchDepthToDefault();
+		_D3D->TurnOnCulling();
+	}
+	Math::SetTranslation(modSkybox.transform, _cameras[0].GetCameraMatrix().Translation());
+	///REFLECTION CUBE MAP DONE
+	
 
 	///RENDERING UI
 	_deviceContext->RSSetViewports(1, &_D3D->viewport);
@@ -289,14 +301,12 @@ bool Renderer::RenderFrame(float dTime){
 		_cameras[0].GetCameraMatrix().Translation(), dTime);
 	modBall.Draw(_deviceContext, shaderLight);
 	shaderLight.ReleaseShaderParameters(_deviceContext);
-
+	///RENDERING DEPTH TEXTURE DONE
 
 	///RENDERING TERRAIN
 	_deviceContext->ClearDepthStencilView(_D3D->GetDepthStencilView(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
 	_deviceContext->RSSetViewports(1, &_D3D->viewport);
 	_D3D->SetBackBufferRenderTarget();
-
-	///RENDERING DEPTH TEXTURE DONE
 
 	for (auto tm : _terrainModels) {
 		shaderShadow.SetShaderParameters(_deviceContext, *tm, _cameras[0].GetViewMatrix(), offScreenTexture._view, _cameras[0].GetProjectionMatrix(),
@@ -304,6 +314,7 @@ bool Renderer::RenderFrame(float dTime){
 		tm->Draw(_deviceContext, shaderShadow);
 		shaderShadow.ReleaseShaderParameters(_deviceContext);
 	}
+	///RENDERING TERRAIN DONE
 
 	///RENDERING WIREFRAME
 	_D3D->TurnOnAlphaBlending();
@@ -313,7 +324,7 @@ bool Renderer::RenderFrame(float dTime){
 	_D3D->TurnOffAlphaBlending();
 	///RENDERING WIREFRAME DONE
 
-	///RENDERING TERRAIN DONE
+	
 
 
 	///RENDERING REFLECTION SPHERE/*
--- Engine/strifeps.hlsl
+++ Engine/strifeps.hlsl
@@ -45,7 +45,7 @@ float noise(in float3 x) {
 
 	float2 uv = (p.xy + float2(370.0f, 170.0f) * p.z) + f.xy;
 
-	float3 rg = white.Sample(CloudSampler, (uv + 0.5f) / 256.0f);
+	float3 rg = white.SampleLevel(CloudSampler, (uv + 0.5f) / 256.0f, 0);
 	//float3 rg = white.Load(int3(uv.x, uv.y, 0.0f)).x;
 
 	return -1.0 + 2.0 * lerp(rg.r, rg.g, f.b);	//return 1 - noise value
@@ -55,7 +55,7 @@ float sampleDensityOfMedium(in float3 pos) {
 
 	//octave 1
 	float3 UVW = pos * 0.01f; // Let’s start with some low frequency
-	float Amplitude = 1.0f;
+	float Amplitude = 5.0f;
 	float V = Amplitude * noise(UVW);
 
 	//octave 2
@@ -107,7 +107,6 @@ float3 computeAmbientColour(in float3 pos, in float extK) {
 	return IsotropicScatteringTop + IsotropicScatteringBottom;
 }
 
-
 float4 myMarch(in float3 rayOrigin, in float3 rayDir, in int numSteps) {
 
 	//set up
@@ -123,9 +122,9 @@ float4 myMarch(in float3 rayOrigin, in float3 rayDir, in int numSteps) {
 		float3 offset = float3(elapsed, elapsed, elapsed) * timeK;
 		float density = sampleDensityOfMedium(pos + offset);	// Sample perlin/worley/white noise and clamp the result between 0 and 1
 		float scatteringK = 0.1f * density;			//scatteringFactor
-		float extinctionK =  0.01f * density;			//extinctionFactor
+		float extinctionK =  0.1f * density;			//extinctionFactor
 
-		extinction *= exp(-extinctionK * stepSize);	//pass step sign - faster to calculate once and keep passing it!
+		extinction *= exp(-extinctionK);	// * stepSize
 
 		//compute light added by scattering
 		float3 moonCol = computeMoonColour(pos);	//moonlight color arriving at the position
@@ -147,19 +146,19 @@ float4 myMarch(in float3 rayOrigin, in float3 rayDir, in int numSteps) {
 //fractal brownian motion across the texture
 float map5(in float3 p) {
 
-	float3 q = p - float3(0.3, 0.1, 0.2) * sin(elapsed * 0.314159f);
+	float3 q = p - float3(0.3, 0.1, 0.2) * elapsed * 0.314159f;
 	float f = 0.50000f  * noise(q);
 
-	q = q * 2.02f;
+	q = q * FREQUENCY_FACTOR;
 	f += 0.25000f * noise(q);
 
-	q = q * 2.03f;
+	q = q * FREQUENCY_FACTOR;
 	f += 0.12500f * noise(q);
 
-	q = q * 2.01f;
+	q = q * FREQUENCY_FACTOR;
 	f += 0.06250f * noise(q);
 
-	q = q * 2.02f;
+	q = q * FREQUENCY_FACTOR;
 	f += 0.03125f * noise(q);
 
 	return clamp(1.5f - p.y - 2.0f + 1.75f * f, 0.0f, 1.0f);
@@ -168,8 +167,8 @@ float map5(in float3 p) {
 
 float4 integrate(in float4 sum, in float dif, in float den, in float t) {
 	// lighting
-	float3 lin = float3(0.65f, 0.7f, 0.75f) * 1.4f + float3(1.0f, 0.6f, 0.3f) * dif;
-	float4 col = float4(lerp(float3(1.0f, 0.95f, 0.8f), float3(0.25f, 0.3f, 0.35f), den), den);
+	float3 lin = dlc * 1.4f + slc * dif;
+	float4 col = float4(lerp(slc, alc, den), den);
 	col.xyz *= lin;
 	col.xyz = lerp(col.xyz, BG_COL, 1.0 - exp(-0.003f * t * t));
 	// front to back blending    
@@ -179,22 +178,24 @@ float4 integrate(in float4 sum, in float dif, in float den, in float t) {
 }
 
 
-void march(in int steps, in float3 rayOrigin, in float3 rayDir, in float initialT, inout float4 sum) {
+void march(in int steps, in float3 rayOrigin, in float3 rayDir, inout float4 sum) {
 
 	float density;
-	float t = initialT;
+	float t = 0.0f;
+	float stepSize = 1.0f / steps;
 
 	for (int i = 0; i < steps; i++) {
 		float3 pos = t * rayDir;
 		if (pos.y < -3.0f || pos.y > 2.0f || sum.a > 0.99f) break;
-		density = map5(pos);
+		if (sum.a > 0.99f) break;
+		density = map5(pos % 256);
 
 		if (density > 0.01f) {
 			//dif is the difference between current density and density of the area closer to the light, clamped 0 to 1
 			float dif = clamp((density - map5(pos - 0.3f * lightDir.xyz)) / 0.6f, 0.0f, 1.0f);
 			sum = integrate(sum, dif, density, t);
 		}
-		t += max(0.05f, 0.02f * t);
+		t += stepSize; //max(0.05f, 0.02f * t);
 	}
 
 	//if (density < 0.33f) discard;
@@ -208,9 +209,9 @@ float4 raymarch(in float3 rayOrigin, in float3 rayDir) {
 
 	int2 xy = rayOrigin.xz;
 
-	float t = perlin.Load(int3(xy.x, xy.y, 0.0f)).x;	//x because it's grayscale so any value would do...
+	//float t = perlin.Load(int3(xy.x, xy.y, 0.0f)).x;	//x because it's grayscale so any value would do...
 
-	march(100, rayOrigin, rayDir, t, sum);
+	march(50, rayOrigin, rayDir, sum);
 
 	return clamp(sum, 0.0f, 1.0f);
 }
@@ -231,7 +232,7 @@ float4 render(in float3 rayOrigin, in float3 rayDir) {
 	background = background * bgAlpha + cloudColour.rgb;	//background colour is "overwritten" by cloud colour
 
 
-	return float4(background, 1.0f - bgAlpha * bgAlpha * bgAlpha);
+	return float4(background, pow(cloudColour.a, 5));
 }
 
 
